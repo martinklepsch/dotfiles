24465f14fb5e663ef1474decf037cba581bac126
Martin Klepsch <martinklepsch@googlemail.com>
2015-02-14 18:02:23 +0100

    fix inf-clojure mess

--- emacs.d/init.d/90-clojure.el
+++ emacs.d/init.d/90-clojure.el
@@ -1,10 +1,11 @@
 (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
 
 (add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
-(add-hook 'inf-clojure-mode-hook #'clojure-mode)
+(add-hook 'inf-clojure-mode-hook #'clojure-font-lock-setup)
+(add-hook 'inf-clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'inf-clojure-mode-hook
           (lambda ()
-            (setq-default show-trailing-whitespace t)))
+            (setq-default show-trailing-whitespace nil)))
 
 (require 'rainbow-delimiters nil)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
@@ -17,4 +18,4 @@
   (insert "(user/reset)")
   (cider-repl-return))
 
-(setq inf-clojure-program "boot repl -c")
+(setq inf-clojure-program "boot repl")
