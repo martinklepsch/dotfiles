8ff4507ee3362a26a8642cdc15e870ff692c11c4
Martin Klepsch <martinklepsch@googlemail.com>
2017-05-09 12:22:24 +0800

    install sjl/z as part of system-setup

--- config/fish/functions/system-setup.fish
+++ config/fish/functions/system-setup.fish
@@ -1,3 +1,7 @@
 function system-setup
-	defaults write com.apple.screencapture location ~/Library/Mobile\ Documents/com~apple~CloudDocs/Screenshots
+  echo "Setting screenshot location to folder in iCloud Drive..."
+  defaults write com.apple.screencapture location ~/Library/Mobile\ Documents/com~apple~CloudDocs/Screenshots
+
+  echo "Downloading and installing z from https://github.com/sjl/z-fish ..."
+  curl https://raw.githubusercontent.com/sjl/z-fish/de2dec23e46ae8620bf2b2d90eea924021107407/z.fish > ~/.config/fish/functions/z.fish
 end
