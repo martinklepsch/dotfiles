56ff3ca651b715912ffef7dc5495c3261acfe366
Martin Klepsch <martinklepsch@googlemail.com>
2018-08-13 17:27:31 +0200

    add note to mplus install script

--- bin/install-mplus.sh
+++ bin/install-mplus.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script installs the M+ font which is my preferred font
+# to use Emacs with. Since emacs fails if it tries to use a font
+# that doesn't exist this is often one of the first things I do
+# on a new desktop system.
+#
+# Note that the script copies files to /Library/Fonts so it may
+# not be suitable to non-Mac systems.
+
 set -eou pipefail
 
 version="mplus-TESTFLIGHT-063"
