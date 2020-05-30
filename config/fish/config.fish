1b14f1a629f4d85a2967fde545c403008567488a
Martin Klepsch <martinklepsch@googlemail.com>
2020-05-30 15:09:24 +0200

    install fisher automatically

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,10 @@
+# install fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # Useful aliases {{{
 
 alias c 'clear'
