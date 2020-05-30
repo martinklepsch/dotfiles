2f2f668f20b07a4b30e18dc2affe381f0b3a0606
Martin Klepsch <martinklepsch@googlemail.com>
2020-05-30 05:38:17 +0200

    add deps.lua

---
+++ hammerspoon/deps.lua
@@ -0,0 +1,20 @@
+-- deps.lua - all-in-one declarative dependency handling
+-- https://gist.github.com/martinklepsch/49ed53cc18879fa1055b9bdd77471c95
+-- vim: sw=2 tw=2 et
+deps = {
+  -- probably should not be using master branches here
+  ["fennel.lua"] = "https://github.com/bakpakin/Fennel/blob/master/fennel.lua",
+  ["moses.lua"] = "https://raw.githubusercontent.com/Yonaba/Moses/master/moses.lua"
+}
+
+for f, url in pairs(deps) do
+  local file_exists, _, code = os.execute("test -f " .. f)
+  if not file_exists then
+    local cmd = "curl -sL -o " .. f .. " " .. url
+    print(cmd)
+    os.execute(cmd)
+    print("You might want to add the downloaded file to your .gitignore")
+  else
+    -- print("exists")
+  end
+end
