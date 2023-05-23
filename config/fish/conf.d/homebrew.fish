92dd67130fd14e8017bd269d7aedfcb6ad7c7817
Martin Klepsch <martinklepsch@googlemail.com>
2023-05-23 23:10:10 +0200

    add homebrew config to config/fish/conf.d

---
+++ config/fish/conf.d/homebrew.fish
@@ -0,0 +1,6 @@
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
